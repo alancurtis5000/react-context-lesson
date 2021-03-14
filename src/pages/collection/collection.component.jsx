c10035316caf8ec0e96ebdd1410bbafbedf48623
Alan <alancurtis5000+accounts@gmail.com>
2021-03-14 13:39:43 -0600

    coverted mapState to useContext

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from "react";
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionsContext from "../../contexts/collections/collections.context";
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import "./collection.styles.scss";
 
-import './collection.styles.scss';
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
 
-const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
       </div>
@@ -21,8 +22,41 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+export default CollectionPage;
 
-export default connect(mapStateToProps)(CollectionPage);
+/*
+// one way of wrapping context. 
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionsContext from "../../contexts/collections/collections.context";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ match }) => {
+  return (
+    <CollectionsContext.Consumer>
+      {(collections) => {
+        const collection = collections[match.params.collectionId];
+        const { title, items } = collection;
+        return (
+          <div className="collection-page">
+            <h2 className="title">{title}</h2>
+            <div className="items">
+              {items.map((item) => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </div>
+          </div>
+        );
+      }}
+    </CollectionsContext.Consumer>
+  );
+};
+
+export default CollectionPage;
+
+
+*/
