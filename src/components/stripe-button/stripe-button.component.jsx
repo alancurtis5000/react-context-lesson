8f9da677e97acf2d6b86f9fddde2484aee6bf32c
Alan <alancurtis5000+accounts@gmail.com>
2021-03-14 12:58:03 -0600

    updated firebase.utils and stripe publishableKey

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51IIIXYFWmiyq6LwEcmhGkqlJLzr72EroFni6XdD8XVA9uQXNYhouPCcy4ukvSgkMGsFVjOlsR4BWxMSRsDuYBG8M00VHOVG4eL';
 
   const onToken = token => {
     console.log(token);
